cc216c66d4cc327d68203ddd436cc9b98a7a7922
karavani <noam12882@gmail.com>
2022-02-23 16:36:56 +0200

    make it better

---
+++ main.js
@@ -0,0 +1,11 @@
+function handleTicketPusrchase(){
+   
+    let text = 'היי אני מעוניין בלרכוש כרטיס למסיבה שלכם, זה הפרטים שלי: \n'; 
+    text += 'שם:'+ document.querySelector("input[name='name']").value
+    text += '\n טלפון:'+ document.querySelector("input[name='phone']").value
+    text += '\n ת.ז:'+ document.querySelector("input[name='id']").value
+    text += '\n הגעתי דרך:'+ document.querySelector("input[name='directby']").value
+
+      window.location.href = `https://wa.me/972557290867?text=${text}`;
+
+}
